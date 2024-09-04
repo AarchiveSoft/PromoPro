a1dc6850808a9ae535646fd7a233dd55f9a23b64
AaronTheGenerous <aaron@windowslive.com>
2024-09-04 16:42:45 +0200

    Add SQLite integration and extend promo scraping logic
    Introduced SQLite database to store scraped promotion data and adjusted code to initialize and populate the database. Enhanced the GetPromos class to include a comprehensive scraping routine for promotions, gathering more detailed information for each category and storing it into the database.

--- main.py
+++ main.py
@@ -6,22 +6,44 @@ Filter by Product
 Filter by Date
 Show all - with sorting option
 """
-
-import sys
+import datetime
 import os
+import sqlite3
+import sys
 import time
 
-from PyQt6 import QtCore, QtWidgets
-from PyQt6.QtWidgets import QWidget, QMainWindow
+from PyQt6.QtWidgets import QWidget
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+
+# create (if necessary) and fill DB
+conn = sqlite3.connect('promoProData.db')
+cursor = conn.cursor()
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS promotions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            brand TEXT NOT NULL,
+            model TEXT,
+            promo_details TEXT,
+            start_date DATE,
+            end_date DATE,
+            scraped_date DATE
+    )
+""")
+conn.commit()
+
 
 class App(QWidget):
+    """
+    Initializes the user interface by creating the GUI and setting up the mode selection.
+    """
+
     def __init__(self):
         super().__init__()
         self.title = "PromoPro"
@@ -33,6 +55,11 @@ class App(QWidget):
         self.initUI()
 
     def initUI(self):
+        """
+        Initializes the user interface by creating the GUI and setting up the mode selection.
+
+        :return: None
+        """
         # create GUI
         # mode selection
         self.setWindowTitle(self.title)
@@ -40,7 +67,29 @@ class App(QWidget):
 
         return
 
+
 class GetPromos():
+    """Class for scraping promotion data from a website and storing it in a SQLite database.
+
+    Args:
+        None
+
+    Attributes:
+        chromedriver_path (str): Path to the Chrome driver executable.
+        chrome_options (str): Options for running Chrome browser.
+        service (webdriver.chrome.Service): Service for running Chrome driver.
+        driver (webdriver.chrome.WebDriver): Chrome driver instance.
+        links (List[str]): List of URLs for each brand's promotion page.
+        conn (sqlite3.Connection): Connection to the SQLite database.
+        cursor (sqlite3.Cursor): Cursor used for executing SQL queries.
+
+    Methods:
+        None
+
+    Usage:
+        get_promos = GetPromos()
+    """
+
     # selenium driver setup - CHROME
     if getattr(sys, "frozen", False):
         # Running as packaged executable, driver is in same directory
@@ -64,24 +113,88 @@ class GetPromos():
     except Exception as e:
         print(f"there was an issue with the driver: {e}")
 
+    # set webdriverwait
+    wait = WebDriverWait(driver, 10)
+
     # go through pages and gather information
     # list of all brands
-    links = []
+    category_links = []
 
-    brand_list = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                                                  ".subcategories-listing-container")))
+    brand_list = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,
+                                                              ".subcategories-listing-container")))
     brands = brand_list.find_elements(By.TAG_NAME, "li")
 
     for brand in brands:
         link_element = brand.find_element(By.TAG_NAME, "a")
         # DEBUG
-        title = link_element.get_attribute("title")
-        link = link_element.get_attribute("href")
-        links.append(link)
-        print(f"link for <{title}> added")
+        category_title = link_element.get_attribute("title")
+        category_link = link_element.get_attribute("href")
+        category_links.append(category_link)
+        print(f"category_link for <{category_title}> added")
 
-    for i in links:
+    # scrape data for each category_link
+    for i in category_links:
+        # get category
         driver.get(i)
-        time.sleep(0.5)
+
+        # get products in list of promo included stuff
+        product_list = wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, ".row.product-filter-target.productlist.productlist-viewmode.productlist-viewmode-list")))
+        products = product_list.find_elements(By.CSS_SELECTOR, "div.content-container")
+
+        # go through products and get various info on each product
+        first_link_flag = False
+        first_link = ""
+        for product in products:
+            link_title_element = product.find_element(By.TAG_NAME, "a")
+            product_title = link_title_element.get_attribute("title")
+            product_link = link_title_element.get_attribute("href")
+
+            if not first_link_flag:
+                first_link = product_link
+                first_link_flag = True
+
+            price_tax_element = product.find_element(By.CLASS_NAME, "price-tax")
+
+            art_nr_element = price_tax_element.find_element(By.CLASS_NAME, "tax-shipping-hint")
+            art_nr_text = art_nr_element.text
+
+            if "Art.Nr.:" in art_nr_text:
+                art_nr = art_nr_text.split("Art.Nr.:")[1].split("\n")[0].strip()
+
+            # debug for now
+            # print(f"scraped info for {product}: \nTitle: {product_title} \nProduct Link: {product_link} \nArt.-Nr.:
+            # {art_nr}")
+
+        # get additional information from the first product (these infos should remain the same over all articles in
+        # the category
+        driver.get(first_link)
+        promo_until = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".specialprice")))
+        promo_until_text = promo_until.text
+        promo_until_text_date = promo_until_text.split("bis:")[1].strip()
+        promo_until_date = time.strptime(promo_until_text_date, "%d.%m.%Y")
+        promo_until_datetime = datetime.datetime(*promo_until_date[:6])
+        promo_until_date_string = promo_until_datetime.strftime('%Y-%m-%d')
+
+        current_time = datetime.datetime.now()
+        current_time_string = current_time.strftime('%Y-%m-%d')
+
+        # print(f"promo until date extracted: {promo_until_date}")
+
+        first_link_flag = False
+        first_link = ""
+
+        # fill db with scraped data:
+        cursor.execute("""
+            INSERT INTO promotions (brand, model, promo_details, start_date, end_date, scraped_date)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, (category_title, product_title, '', '1900-01-01', promo_until_date_string, current_time_string))
+        conn.commit()
+
+        # print table for now for testing
+        cursor.execute('SELECT * FROM promotions')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
 
     driver.quit()
